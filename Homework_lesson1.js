13988fe14978122b2b0111aa6b4afeb1a060cf15
ArtemKhil <artemkhilchenko09@gmail.com>
2021-11-10 21:01:11 +0000

    Lesson1 has been done.

---
+++ Homework_lesson1.js
@@ -0,0 +1,120 @@
+// -Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+// let greeting = 'hello';
+// let school = 'owu';
+// let user = 'com';
+// let country = 'ua';
+// let a = 1;
+// let b = 10;
+// let c = -999;
+// let d = 123;
+// let e = 3.14;
+// let f = 2.7;
+// let g = 16;
+// let car = true;
+// let skill = false;
+
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+//console.log(greeting, school, user, country, a, b, c, d, e, f, g, status, skill);
+// alert(greeting);
+// alert(school);
+// alert(user);
+// alert(country);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(status);
+// alert(skill);
+//document.write(greeting, school, user, country, a, b, c, d, e, f, g, status, skill);
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+
+// greeting = 'Good morning';
+// school = 'Slade';
+// user = 'Artem';
+// country = 'Ukraine';
+// a = 4;
+// b = 7;
+// c = -54;
+// d = 56;
+// e = 54;
+// f = 897;
+// g = 578;
+// car = false;
+// skill = true;
+
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+//console.log(greeting, school, user, country, a, b, c, d, e, f, g, status, skill);
+// alert(greeting);
+// alert(school);
+// alert(user);
+// alert(country);
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(e);
+// alert(f);
+// alert(g);
+// alert(status);
+// alert(skill);
+//document.write(greeting, school, user, country, a, b, c, d, e, f, g, status, skill);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+
+// let firstName = 'Artem';
+// let middleName = 'Junior';
+// let lastName = 'Khilchenko';
+// let age = 34;
+// let person = firstName + middleName + lastName;
+// console.log(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+
+// prompt('What is your name?');
+// prompt('What is your Surname?');
+// prompt('How old are you?');
+
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+//document.write('Hello' + ' ' + firstName + ' ' + lastName + '.' + 'You are' + ' ' + age + ' ' + 'years old'+'.');
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// console.log(typeof a);
+// let b = '100';
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+
+//     5 < 6 -> true
+// 5 == 6 -> false
+// 5 === 6 -> false
+// 5 > 6 -> false
+// 10 == 10 -> true
+// 10 === 10 -> true
+// 10 < 10 -> false
+// 10 > 10 -> false
+// 10 !== 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//  let str = "20";
+//  let a = 5;
+//   document.write(str + a + "<br/>");  '205'  string;
+//   document.write(str - a + "<br/>");   15    number;
+//   document.write(str * "2" + "<br/>"); 40    number;
+//   document.write(str / 2 + "<br/>");   10    number;
